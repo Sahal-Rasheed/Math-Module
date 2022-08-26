40ce832e0c437eaaea227c492712a0a68a5cd4a3
Sahal-Rasheed <110896428+Sahal-Rasheed@users.noreply.github.com>
2022-08-26 14:17:51 +0530

    Add files via upload

---
+++ math1.py
@@ -0,0 +1,11 @@
+import math
+
+print(math.pi)
+print(math.inf)
+
+print("__________________________________")
+print(math.ceil(1.4))
+print(math.ceil(-5.3))
+print("__________________________________")
+print(math.floor(1.5))
+print(math.floor(-5.6))
