af6523a433d7eeef7667cdf8fad9032c53855dba
Sahal-Rasheed <110896428+Sahal-Rasheed@users.noreply.github.com>
2022-08-26 14:39:26 +0530

    Add files via upload

---
+++ math2.py
@@ -0,0 +1,17 @@
+import math
+print(math.cos(0.00))
+print(math.cos(1))
+print(math.cos(10))
+print("__________________________________")
+print(math.sin(0))
+print(math.sin(1))
+print(math.sin(10))
+print("__________________________________")
+print(math.degrees(8.90))
+print(math.degrees(1))
+print(math.degrees(10))
+print("__________________________________")
+print(math.fabs(-10))
+print(math.fabs(-5.9))
+print(math.fabs(5.7))
+
